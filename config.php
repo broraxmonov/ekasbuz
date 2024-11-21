fe0b58a88d0f062313f3afb989784f243014c697
broraxmonov <bro.raxmonov@gmail.com>
2024-11-22 01:26:58 +0500

    Update config.php
    dasdasd

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('Asia/Tashkent');
 
-define('API_KEY','Token');
+define('API_KEY','8120035468:AAG4crs__k-KCE3fSiRIOXIfLziUYc0wgRc');
 // admin akkounti id raqamini ushbu bot orqali bilishingiz mumkin @infomiruz_idbot
-$Manager = "admin id";
-$company_name = "bot yoki tizim nomi";
+$Manager = "6287368227";
+$company_name = "ekasbuz";
 $logging = true; 
 $local_db_sqlite = [
     "status" => true
