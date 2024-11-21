00af2d0be2ccc07f93bc2eeaf940d696a023e4a2
broraxmonov <bro.raxmonov@gmail.com>
2024-11-22 01:40:35 +0500

    Update bot.php
    boott

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BOT_TOKEN', '12345678:replace-me-with-real-token');
+define('BOT_TOKEN', '8120035468:AAG4crs__k-KCE3fSiRIOXIfLziUYc0wgRc');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
